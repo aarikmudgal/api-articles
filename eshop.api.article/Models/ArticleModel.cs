a3d4bdbe872936c45bc57344361e4d7b921a28a6
Aarik Mudgal <Aarik_Mudgal@ad.infosys.com>
2018-03-31 13:17:51 +0530

    changes

--- eshop.api.article/Models/ArticleModel.cs
+++ eshop.api.article/Models/ArticleModel.cs
@@ -4,12 +4,12 @@
     {
         public Article[] Articles { get; set; }
     }
-    public class Article
-    {
-        public string ArticleId { get; set; }
-        public string ArticleName { get; set; }
-        public string ArticleDescription { get; set; }
-        public string ArticlePrice { get; set; }
-        public string ArticleImageUrl { get; set; }
-    }
+    //public class Article
+    //{
+    //    public string ArticleId { get; set; }
+    //    public string ArticleName { get; set; }
+    //    public string ArticleDescription { get; set; }
+    //    public string ArticlePrice { get; set; }
+    //    public string ArticleImageUrl { get; set; }
+    //}
 }
